75e0e4f42a78f388641aba474d6171912c4bafde
Ted Lilley <ted.lilley@digi.com>
2023-12-02 21:31:27 -0500

    day 1

---
+++ day1_test.go
@@ -0,0 +1,111 @@
+package aoc2023
+
+import (
+	"bufio"
+	"os"
+	"testing"
+)
+
+func TestSumWords(t *testing.T) {
+	t.Parallel()
+
+	type args struct {
+		words []string
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "eleven",
+			args: args{
+				words: []string{"a1c1w"},
+			},
+			want: 11,
+		},
+		{
+			name: "single number eleven",
+			args: args{
+				words: []string{"a1w"},
+			},
+			want: 11,
+		},
+		{
+			name: "actual",
+			args: args{
+				words: loadFile("day1_input.txt"),
+			},
+			want: 55108,
+		},
+	}
+
+	for _, test := range tests {
+		tt := test
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			if got := SumWords(tt.args.words); got != tt.want {
+				t.Errorf("SumWords() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func loadFile(fileName string) []string {
+	file, err := os.Open(fileName)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var lines []string
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err = scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	return lines
+}
+
+func TestSumNumWords(t *testing.T) {
+	t.Parallel()
+
+	type args struct {
+		words []string
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "actual",
+			args: args{
+				words: loadFile("day1_input.txt"),
+			},
+			want: 55108,
+		},
+	}
+
+	for _, test := range tests {
+		tt := test
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			if got := SumNumWords(tt.args.words); got != tt.want {
+				t.Errorf("SumNumWords() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/binaryphile/aoc2023
 
 go 1.21.0
+
+require (
+	github.com/mariomac/gostream v0.8.1 // indirect
+	golang.org/x/exp v0.0.0-20220328175248-053ad81199eb // indirect
+)
